a84af418390f1488dce96a48774c003a34bce73e
lcsdeveloper <lucassouzadeveloper@gmail.com>
2017-03-14 12:10:00 -0300

    Primeiros Códigos

---
+++ ProgramasJava/src/programasjava/OperadoresAritimeticos.java
@@ -0,0 +1,11 @@
+/*Programa sobre os operadores aritimeticos em Java*/
+package programasjava;
+public class OperadoresAritimeticos {
+    public static void main(String[] args){
+       //variaveis
+    int n1 = 3, n2 = 5;
+    float m = (n1 + n2)/2;
+    //display
+    System.out.println("A média é igual a: " + m); 
+    }     
+}
---
+++ ProgramasJava/src/programasjava/olamundo.java
@@ -0,0 +1,6 @@
+package programasjava;
+public class olamundo {
+     public static void main(String[] args) {
+        System.out.println("Ola, mundo!");
+    }
+}
