a0005d1a12ef79928a2ecab3ba8481717d555fee
lcsdeveloper <lucassouzadeveloper@gmail.com>
2017-03-17 12:24:06 -0300

    Comecando OOP com Java

---
+++ GradeBook/src/gradebook/GradeBook.java
@@ -0,0 +1,10 @@
+//declaracao da Classe com um metodo
+//Lucas Souza
+//Create 17/03/2017 12:00 am
+package gradebook;
+public class GradeBook {
+    //exibe uma mensagem para o usuario do Grade Book
+    public void displayMessage(){
+        System.out.println("Welcome to the Grande Book!");
+    }//fim do metodo displayMessage
+}//fim da classe GradeBook
---
+++ GradeBook/src/gradebook/GradeBookTest.java
@@ -0,0 +1,14 @@
+//criando um objeto GradeBook e chamando o seu metodo displayMessage
+//Lucas Souza
+//13/03/2017 12:08 am
+package gradebook;
+public class GradeBookTest {
+    //metodo main que inicia a execucao do programa
+    public static void main( String[] args ){
+        //instanciando o objeto GradeBook e o atribuindo a myGradeBook
+        GradeBook myGradeBook = new GradeBook();
+        
+        //chamando o metodo displayMessage de myGradeBook
+        myGradeBook.displayMessage();
+    }//fim do metodo main
+}//fim da classe GradeBookTest
